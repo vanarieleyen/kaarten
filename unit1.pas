d356f228b7bf3d902ceb92eda3bb51886fcecc0d
arievanleyen <vanarieleyen@riseup.net>
2017-02-09 11:59:59 +0800

    do

--- unit1.pas
+++ unit1.pas
@@ -6,8 +6,8 @@ interface
 
 uses
   Classes, SysUtils, FileUtil, Forms, Math, Controls, Graphics, Dialogs,
-  ExtCtrls, StdCtrls, LazLogger, BGRABitmap, BGRABitmapTypes,
-  BGRACanvas2D, FPimage;
+  ExtCtrls, StdCtrls, LazLogger, BGRAVirtualScreen, BGRABitmap, BGRABitmapTypes,
+  BGRACanvas2D, FPimage, BCTypes;
 
 type
 
@@ -20,14 +20,14 @@ type
   end;
 
   TForm1 = class(TForm)
+    VirtualScreen: TBGRAVirtualScreen;
     Button1: TButton;
-    deck_png: TImageList;
-    felt_png: TImageList;
+    cardlist: TImageList;
+    procedure VirtualScreenClick(Sender: TObject);
+    procedure VirtualScreenRedraw(Sender: TObject; Bitmap: TBGRABitmap);
     procedure Button1Click(Sender: TObject);
-    procedure FormClick(Sender: TObject);
     procedure FormCreate(Sender: TObject);
     procedure FormDestroy(Sender: TObject);
-    procedure FormPaint(Sender: TObject);
     procedure FormResize(Sender: TObject);
 
   private
@@ -35,6 +35,7 @@ type
     virt: TBGRABitmap;  // virtual screen
     mask: TBGRABitmap;  // mask of the drawn hand, used to recognize the selected card
     side: integer;      // canvas size for one card that allows maximum rotation without clipping
+    bm: TBitmap;
     hand: array of THand;
   public
     procedure drawCard(scale: double; myhand: THand);
@@ -51,10 +52,10 @@ const
   HEARTS = 3;
   SPADES = 4;
   CARDHEIGHT = 312;
-  CARDWIDTH = 224;
+  CARDWIDTH = 223;
   HALFCARDHEIGHT = round(CARDHEIGHT / 2);
   HALFCARDWIDTH = round(CARDWIDTH / 2);
-  SEGMENT = 40;    // hand is displayed in a circle of 40 degrees
+  SEGMENT = 80;    // hand is displayed in a circle of 40 degrees
 
 implementation
 
@@ -63,14 +64,9 @@ implementation
 { TForm1 }
 
 procedure TForm1.FormCreate(Sender: TObject);
-var
-  bm: TBitmap;
 begin
   bm := TBitmap.Create;
-  deck_png.GetBitmap(0, bm);
-  deck := TBGRABitmap.Create(bm);
   side := round(sqrt(power(CARDWIDTH, 2) + power(CARDHEIGHT, 2)));
-  FreeAndNil(bm);
 end;
 
 procedure TForm1.FormDestroy(Sender: TObject);
@@ -78,10 +74,11 @@ begin
   FreeAndNil(deck);
   FreeAndNil(mask);
   FreeAndNil(virt);
+  FreeAndNil(bm);
 end;
 
 // select a card on the hand and refresh the display
-procedure TForm1.FormClick(Sender: TObject);
+procedure TForm1.VirtualScreenClick(Sender: TObject);
 var
   pt: TPoint;
   id: integer;
@@ -89,18 +86,14 @@ var
 begin
   pt := ScreenToClient(Mouse.CursorPos);
   pixel := Mask.GetPixel(pt.x, pt.y);
-  id := pixel.red;
+  id := pixel.red shr 4;
   DebugLn(IntToStr(hand[id].suit) + ' ' + IntToStr(hand[id].rank));
 
-  if (hand[id].suit > 0) and (hand[id].rank > 0) then
-  begin
-    if hand[id].bid = True then
-    begin
+  if (hand[id].suit > 0) and (hand[id].rank > 0) then begin
+    if hand[id].bid = True then begin
       hand[id].x -= round(40 * sin(hand[id].angle));
       hand[id].y += round(40 * cos(hand[id].angle));
-    end
-    else
-    begin
+    end else begin
       hand[id].x += round(40 * sin(hand[id].angle));
       hand[id].y -= round(40 * cos(hand[id].angle));
     end;
@@ -108,21 +101,27 @@ begin
 
     drawBackground();
     drawHand();
-    Refresh;
+    VirtualScreen.RedrawBitmap;
   end;
 end;
 
 // displays the mask on the form
 procedure TForm1.Button1Click(Sender: TObject);
 begin
-  Canvas.CopyRect(Canvas.ClipRect, Mask.Canvas, Canvas.ClipRect);
+  {$IFDEF Windows}
+    virt.GetImageFromCanvas(mask.Canvas,0,0);
+  {$ELSE}
+    virt.Assign(mask);
+  {$ENDIF}
+  VirtualScreen.RedrawBitmap;
 end;
 
 // displays the content of the virtual canvas on the form
-procedure TForm1.FormPaint(Sender: TObject);
+procedure TForm1.VirtualScreenRedraw(Sender: TObject; Bitmap: TBGRABitmap);
 begin
-  Canvas.CopyRect(Canvas.ClipRect, virt.Canvas, Canvas.ClipRect);
-  //DebugLn('repaint');
+  DebugLn('redraw');
+  //Bitmap.PutImage(0,0,virt,dmSet,150);
+  Bitmap.Assign(virt.Bitmap);
 end;
 
 // redraws the background and hand on the form
@@ -133,21 +132,22 @@ var
 begin
   drawBackground();
 
-  radius := DegToRad(SEGMENT);
   aantal := 12;
+  radius := DegToRad( SEGMENT/12*aantal );
   step := radius / aantal;
   angle := radius / 2 - radius + step / 2;
   setlength(hand, aantal + 1);
   for i := 1 to aantal do
   begin
     hand[i].id := i;
-    hand[i].x := i * 20;
+    hand[i].x := i * 15;
     hand[i].y := 100;
     hand[i].suit := random(4) + 1;
     hand[i].rank := random(13) + 1;
     hand[i].angle := angle;
     hand[i].bid := False;
     angle += step;
+    DebugLn(inttostr(hand[i].suit)+' '+inttostr(hand[i].rank));
   end;
   drawHand();
 end;
@@ -157,34 +157,25 @@ procedure TForm1.drawHand();
 var
   i: integer;
 begin
-  for i := 1 to length(hand) do
+  for i := 1 to length(hand)-1 do
     drawCard(0.7, hand[i]);
 end;
 
 // draws a card on the virtual canvas
 // draws a mask on the mask canvas (used to determine the clicked card)
-// the cards are all in one large bitmap, this function copies one of those cards
 procedure TForm1.drawCard(scale: double; myhand: THand);
 var
   card, bmp: TBGRABitmap;
   ctx: TBGRACanvas2D;
-  cx, cy, newsize: integer;
+  newsize: integer;
   pixel: TBGRAPixel;
 begin
-  // get the position of the card in the deck_png
-  case myhand.suit of
-    DIAMONDS: cy := 3 * 352;
-    CLUBS: cy := 0;
-    HEARTS: cy := 352;
-    SPADES: cy := 2 * 352;
-  end;
-  cx := (myhand.rank - 1) * 263;
-  if myhand.rank=1 then cx += 1;
-
   newsize := round(side*scale);
 
+  cardlist.GetBitmap(myhand.suit*myhand.rank, bm);
+  card := TBGRABitmap.Create(bm, False);
+
   // draw the card on the virtual canvas
-  card := deck.GetPart(Rect(cx - 1, cy, CARDWIDTH + cx + 2, CARDHEIGHT + cy)) as TBGRABitmap;
   bmp := TBGRABitmap.Create(side, side, BGRAPixelTransparent);
   ctx := bmp.Canvas2D;
   ctx.antialiasing := true;
@@ -194,7 +185,9 @@ begin
   virt.Canvas2d.drawImage(bmp, myhand.x, myhand.y, newsize, newsize);
 
   // draw the mask of the card
-  pixel.red := myhand.id;
+  pixel.red := myhand.id shl 4;
+  pixel.blue:= myhand.suit shl 4; // not used (only for mask coloring)
+  pixel.green:=myhand.rank shl 4; // not used (only for mask coloring)
   pixel.alpha := 255;
   ctx.fillStyle(pixel);
   ctx.roundRect(-HALFCARDWIDTH, -HALFCARDHEIGHT, CARDWIDTH, CARDHEIGHT, 10);
@@ -211,7 +204,6 @@ procedure TForm1.drawBackground();
 var
   X, Y: integer;
   texture: TBGRABitmap;
-  bm: TBitmap;
 begin
   if Assigned(mask) then
     FreeAndNil(mask);
@@ -220,19 +212,16 @@ begin
   mask := TBGRABitmap.Create(Width, Height, BGRAPixelTransparent);
   virt := TBGRABitmap.Create(Width, Height, BGRAPixelTransparent);
 
-  bm := TBitmap.Create;
-  felt_png.GetBitmap(0, bm);
+  cardlist.GetBitmap(0, bm);
   texture := TBGRABitmap.Create(bm, False);
 
-  for X := 0 to (Width div texture.Width) do
-  begin
-    for Y := 0 to (Height div texture.Height) do
-    begin
+  for X := 0 to (Width div texture.Width) do begin
+    for Y := 0 to (Height div texture.Height) do begin
+      //Canvas.Draw(X * texture.Width, Y * texture.Height, texture.Bitmap);
       virt.Canvas2d.drawImage(texture, X * texture.Width, Y * texture.Height);
     end;
   end;
   FreeAndNil(texture);
-  FreeAndNil(bm);
 
   Mask.FillRect(0, 0, Width, Height, BGRAPixelTransparent, dmSet);
 end;
